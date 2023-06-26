602326b5a6f86a518ac00bbdc6df0993b1ba8a2e
Judah <judahben149@gmail.com>
2023-06-26 16:28:11 +0100

    Removed unused code

--- app/src/main/java/com/judahben149/countrycookbook/Apollo.kt
+++
@@ -1,7 +0,0 @@
-package com.judahben149.countrycookbook
-
-import com.apollographql.apollo3.ApolloClient
-
-val apolloClient = ApolloClient.Builder()
-    .serverUrl("https://countries.trevorblades.com")
-    .build()
--- app/src/main/java/com/judahben149/countrycookbook/MainActivity.kt
+++ app/src/main/java/com/judahben149/countrycookbook/MainActivity.kt
@@ -19,11 +19,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//
-//        lifecycleScope.launch {
-//            val response = apolloClient.query(GetAllContinentsQuery()).execute()
-//            Log.d("TAG", "Success ${response.data}")
-//
-//        }
     }
 }
