4ff4b22c45619c9021456d5ace7b0a0391e0c1a5
Judah <judahben149@gmail.com>
2023-08-21 22:39:56 +0100

    Refactored Activity

--- app/src/main/java/com/judahben149/countrycookbook/MainActivity.kt
+++ app/src/main/java/com/judahben149/countrycookbook/MainActivity.kt
@@ -1,23 +1,17 @@
 package com.judahben149.countrycookbook
 
-import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.AttributeSet
-import android.util.Log
-import android.view.View
-import android.widget.TextView
-import androidx.lifecycle.lifecycleScope
-import com.apollographql.apollo3.ApolloClient
-import com.judahben149.GetAllContinentsQuery
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import com.judahben149.countrycookbook.presentation.screens.ContinentListScreen
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent { 
+            ContinentListScreen()
+        }
     }
 }
