573dc36a51c6093d370faec575ca3af19ce4f425
Judah <judahben149@gmail.com>
2023-08-23 01:06:39 +0100

    Refactored continent list composable

--- app/src/main/java/com/judahben149/countrycookbook/presentation/screens/ContinentList.kt
+++ app/src/main/java/com/judahben149/countrycookbook/presentation/screens/ContinentList.kt
@@ -1,8 +1,11 @@
 package com.judahben149.countrycookbook.presentation.screens
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -10,6 +13,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.judahben149.countrycookbook.presentation.components.ItemContinent
 import com.judahben149.countrycookbook.presentation.components.LoadingProgress
@@ -56,14 +60,18 @@ fun ContinentListScreen() {
 fun ContinentListScreenContent(state: ContinentListUIState, onClick: (id: String) -> Unit) {
     val continentList = (state as ContinentListUIState.Loaded).continentList
 
-    LazyColumn {
-        items(
-            count = continentList.size
-        ) { itemIndex ->
-            ItemContinent(
-                continent = state.continentList[itemIndex],
-                onItemClick = { onClick(state.continentList[itemIndex].id) })
+    Surface(modifier = Modifier.fillMaxSize()) {
+        LazyColumn {
+            items(
+                count = continentList.size
+            ) { itemIndex ->
+                ItemContinent(
+                    continent = state.continentList[itemIndex],
+                    onItemClick = { onClick(state.continentList[itemIndex].id) })
+            }
         }
+
+        Spacer(modifier = Modifier.height(16.dp))
     }
 }
 
