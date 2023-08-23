78afdd166d337d21a5a2dccbb7b309a800c243d7
Judah <judahben149@gmail.com>
2023-08-23 20:52:20 +0100

    Modified progress loader layout

--- app/src/main/java/com/judahben149/countrycookbook/presentation/components/LoadingProgress.kt
+++ app/src/main/java/com/judahben149/countrycookbook/presentation/components/LoadingProgress.kt
@@ -1,14 +1,27 @@
 package com.judahben149.countrycookbook.presentation.components
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun LoadingProgress() {
     Box {
         CircularProgressIndicator()
+    }
+}
+
+@Composable
+fun LoadingProgressFilledScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        LoadingProgress()
     }
 }
 
