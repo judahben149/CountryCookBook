eacc4ee4349668ec3508fa7cc4df5f73ee483a79
Judah <judahben149@gmail.com>
2023-08-26 21:52:52 +0100

    Completed setting up Country Item Composable and converted flag image to bitmap

--- app/src/main/java/com/judahben149/countrycookbook/presentation/components/ItemCountry.kt
+++ app/src/main/java/com/judahben149/countrycookbook/presentation/components/ItemCountry.kt
@@ -1,35 +1,57 @@
 package com.judahben149.countrycookbook.presentation.components
 
-import androidx.compose.foundation.layout.Column
+import android.content.Context
+import android.graphics.drawable.BitmapDrawable
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.judahben149.countrycookbook.domain.model.Country
+import com.judahben149.countrycookbook.utils.getEmojiDrawable
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ItemCountry(country: Country, onItemClick: (countryCode: String) -> Unit) {
+fun ItemCountry(context: Context, country: Country, onItemClick: (countryCode: String) -> Unit) {
+
     Card(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 6.dp),
         onClick = { onItemClick(country.id) }
     ) {
-        Column(modifier = Modifier.align(Alignment.CenterHorizontally)) {
-            Text(
-                text = country.name
-            )
 
-            Text(text = country.capital)
-        }
+        Image(
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(top = 8.dp),
+            contentScale = ContentScale.Inside,
+            bitmap = (country.flag.getEmojiDrawable(context) as BitmapDrawable).bitmap.asImageBitmap(),
+            contentDescription = null
+        )
+
+        Text(
+            modifier = Modifier
+                .padding(vertical = 12.dp)
+                .align(Alignment.CenterHorizontally),
+            text = country.name,
+            fontSize = 12.sp
+        )
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun ItemCountryPreview() {
-    ItemCountry(Country().copy(name = "Nigeria", capital = "Abuja"), { })
+//    ItemCountry(Country().copy(name = "Nigeria", capital = "Abuja"), { })
 }
