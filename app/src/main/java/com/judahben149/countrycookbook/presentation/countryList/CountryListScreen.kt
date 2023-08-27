1a429dfc2f96ed4a6f3810ba74291a37b95b03af
Judah <judahben149@gmail.com>
2023-08-27 20:54:29 +0100

    Converted List view to Grid view (Lazy VerticalStaggered Grid)

--- app/src/main/java/com/judahben149/countrycookbook/presentation/countryList/CountryListScreen.kt
+++ app/src/main/java/com/judahben149/countrycookbook/presentation/countryList/CountryListScreen.kt
@@ -1,16 +1,27 @@
 package com.judahben149.countrycookbook.presentation.countryList
 
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.judahben149.countrycookbook.navigation.Screen
 import com.judahben149.countrycookbook.presentation.components.ItemCountry
 import com.judahben149.countrycookbook.presentation.components.LoadingProgressFilledScreen
+import com.judahben149.countrycookbook.presentation.components.TopAppBarComponent
+import com.judahben149.countrycookbook.utils.toContinentName
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
@@ -19,7 +30,7 @@ fun CountryListScreen(
     navController: NavController,
     continentId: String,
 ) {
-    val viewModel = viewModel<CountriesViewModel>()
+    val viewModel: CountriesViewModel = hiltViewModel()
     val state by viewModel.uiState.collectAsState()
 
     LaunchedEffect(key1 = continentId) {
@@ -28,39 +39,57 @@ fun CountryListScreen(
         }
     }
 
-    when(state) {
-        CountryListUIState.Loading -> {
-            LoadingProgressFilledScreen()
-        }
+    Column(modifier = Modifier.fillMaxSize()) {
 
-        is CountryListUIState.Loaded -> {
-            CountryListScreenContent(
-                state = state,
-                onClick = { countryId ->
-                    navController.navigate(
-                        Screen.CountryDetailScreen.route.replace(
-                            "{countryId}",
-                            "${countryId}"
+        TopAppBarComponent(
+            title = "Countries in ${continentId.toContinentName()}",
+            isHomeDestination = false,
+            shouldCollapse = true,
+            onNavIconClick = { navController.popBackStack() }
+        )
+
+        when (state) {
+            CountryListUIState.Loading -> {
+                LoadingProgressFilledScreen()
+            }
+
+            is CountryListUIState.Loaded -> {
+                CountryListScreenContent(
+                    state = state,
+                    onClick = { countryId ->
+                        navController.navigate(
+                            Screen.CountryDetailScreen.route.replace(
+                                "{countryId}",
+                                "${countryId}"
+                            )
                         )
-                    )
-                }
-            )
-        }
+                    }
+                )
+            }
 
-        is CountryListUIState.Error -> {
+            is CountryListUIState.Error -> {
 
+            }
         }
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun CountryListScreenContent(state: CountryListUIState, onClick: (id: String) -> Unit) {
     val countryList = (state as CountryListUIState.Loaded).continent.countryList
 
-    LazyColumn {
+    LazyVerticalStaggeredGrid(
+        modifier = Modifier.fillMaxSize(),
+        columns = StaggeredGridCells.Fixed(2),
+        contentPadding = PaddingValues(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
         items(countryList.size) { countryId ->
             ItemCountry(
                 country = countryList[countryId],
+                context = LocalContext.current,
                 onItemClick = { onClick(countryList[countryId].id) }
             )
         }
